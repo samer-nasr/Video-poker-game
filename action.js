3936feeaa2f7dcabe19f68251b5670c143db0ca5
samer-nasr <samernasr1@live.com>
2023-07-15 01:52:53 +0300

    update

---
+++ action.js
@@ -0,0 +1,40 @@
+
+$("#card1").click(function(){ 
+    if($("#h1").text() == ""){ 
+        $("#h1").html("HELD");
+    }else{ 
+        $("#h1").html("");
+    }
+});
+
+$("#card2").click(function(){ 
+    if($("#h2").text() == ""){ 
+        $("#h2").html("HELD");
+    }else{ 
+        $("#h2").html("");
+    }
+});
+
+$("#card3").click(function(){ 
+    if($("#h3").text() == ""){ 
+        $("#h3").html("HELD");
+    }else{ 
+        $("#h3").html("");
+    }
+});
+
+$("#card4").click(function(){ 
+    if($("#h4").text() == ""){ 
+        $("#h4").html("HELD");
+    }else{ 
+        $("#h4").html("");
+    }
+});
+
+$("#card5").click(function(){ 
+    if($("#h5").text() == ""){ 
+        $("#h5").html("HELD");
+    }else{ 
+        $("#h5").html("");
+    }
+});
